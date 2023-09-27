16be127e4532c9966ed758190e5a78047f339e74
Iqbal Mahdi <mahdiiqbal37@gmail.com>
2023-09-27 19:11:59 +0600

    Updated task 5 - password must contain all 4 types of characters

--- task_05.php
+++ task_05.php
@@ -4,17 +4,43 @@ print("================================ Task 5 ================================\
 
 function generatePassword(int $length = 12): string
 {
-    $characters = str_split("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+");
-
-    $password = "";
-
-    for ($i = 0; $i < $length; $i++)
+    $upperCaseChars = str_split("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    
+    $lowerCaseChars = str_split("abcdefghijklmnopqrstuvwxyz");
+    
+    $numericChars = str_split("0123456789");
+    
+    $specialChars = str_split("!@#$%^&*()_+");
+    
+    $chars = array_merge($upperCaseChars, $lowerCaseChars, $numericChars, $specialChars);
+    
+    while(true)
     {
-        $index = array_rand($characters);
+        $password = "";
 
-        $password .= $characters[$index];
+        $hasUpperCase = $hasLowerCase = $hasNumeric = $hasSpecial = false;
+
+        for ($i = 0; $i < $length; $i++)
+        {
+            $index = array_rand($chars);
+
+            $char = $chars[$index];
+    
+            $password .= $char;
+
+            if ($hasUpperCase == false && in_array($char, $upperCaseChars)) $hasUpperCase = true;
+
+            if ($hasLowerCase == false && in_array($char, $lowerCaseChars)) $hasLowerCase = true;
+
+            if ($hasNumeric == false && in_array($char, $numericChars)) $hasNumeric = true;
+
+            if ($hasSpecial == false && in_array($char, $specialChars)) $hasSpecial = true;
+        }
+
+        if ($hasUpperCase && $hasLowerCase && $hasNumeric && $hasSpecial) break;
+
     }
-
+    
     return $password;
 }
 
